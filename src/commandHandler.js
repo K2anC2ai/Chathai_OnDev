46b5d73ab48b1cefb21cd42fe844a481f51108b0
chatmongkolsawarose <chatmongkol.saw@gmail.com>
2025-05-06 15:37:11 +0700

    completecommand

--- src/commandHandler.js
+++ src/commandHandler.js
@@ -11,7 +11,7 @@ function generateStepCode({ command, value, chaining, chained }) {
       isChained = true;
     }
   
-    else if (chained && ['type', 'click', 'check', 'uncheck', 'select'].includes(command)) {
+    else if (chained && ['type', 'click', 'check', 'uncheck', 'select', 'focus', 'blur', 'clear', 'dblclick', 'rightclick', 'trigger', 'scrollIntoView', 'scrollTo'].includes(command)) {
       code += `.${command}(${formatValue(value)})\n`;
     }
   
@@ -22,7 +22,84 @@ function generateStepCode({ command, value, chaining, chained }) {
     else if (chained && command === 'contains') {
       code += `.contains(${formatValue(value)})\n`;
     }
-  
+
+    else if (chained && ['find', 'parent', 'parents', 'children', 'siblings', 'next', 'prev', 'first', 'last', 'eq', 'filter', 'not'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+      isChained = true;
+    }
+
+    else if (chained && ['wait', 'timeout'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['invoke', 'its'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+      isChained = true;
+    }
+
+    else if (chained && ['as', 'then'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+      isChained = true;
+    }
+
+    else if (chained && ['within', 'each'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+      isChained = true;
+    }
+
+    else if (chained && ['submit', 'reset'].includes(command)) {
+      code += `.${command}()\n`;
+    }
+
+    else if (chained && ['selectFile', 'attachFile'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['drag', 'drop'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['hover', 'mouseover', 'mouseout'].includes(command)) {
+      code += `.${command}()\n`;
+    }
+
+    else if (chained && ['screenshot'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['debug', 'pause'].includes(command)) {
+      code += `.${command}()\n`;
+    }
+
+    else if (chained && ['reload', 'go'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['viewport'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['scrollTo'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['clearCookie', 'clearCookies', 'clearLocalStorage'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['getCookie', 'getCookies'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+      isChained = true;
+    }
+
+    else if (chained && ['setCookie'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
+    else if (chained && ['log'].includes(command)) {
+      code += `.${command}(${formatValue(value)})\n`;
+    }
+
     else {
       code += `    cy.${command}(${formatValue(value)})\n`;
     }
